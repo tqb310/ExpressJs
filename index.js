7cc9bd6de1e575b641cc13f27799e3759ea30a6a
tqb310 <baotq310@gmail.com>
2021-02-20 11:03:37 +0700

    demo pug

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+/*const pug = require('pug');
+
+const CompiledFunction = pug.compileFile('index.pug');
+
+console.log(CompiledFunction({
+	name: 'Truong Quoc Bao',
+}));*/
+
 const express = require('express');
 const app = express();
-
 const port = 3050;
+
+app.set('view engine','pug');
+app.set('views','./views');
+
 app.get('/', (req, res) => {
-	res.send('<h1>Hello World</h1>');
+	res.render('index', {
+		classes: '12A3',
+	});
+});
+
+app.get('/students', (req, res) => {
+	res.render('students/index', {
+		users: [
+			{id: 1, name:'Truong Quoc Bao'},
+			{id: 2, name:'Nguyen Gia Huy'},
+			{id: 3, name:'Tran Quoc Khanh'}
+		]
+	});
 });
 
 app.listen(port, () => {
 	console.log('Server listening on port ' + port);
 });
+
+
+
+
+
+
+
+
