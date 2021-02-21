24d87eb5731df14ff3e58f64557595ae036054d7
tqb310 <baotq310@gmail.com>
2021-02-21 23:05:39 +0700

    demo bodyParser

--- index.js
+++ index.js
@@ -7,37 +7,59 @@ console.log(CompiledFunction({
 }));*/
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3050;
-var users = [
-			{id: 1, name:'Truong Quoc Bao'},
-			{id: 2, name:'Nguyen Gia Huy'},
-			{id: 3, name:'Tran Quoc Khanh'}
+var students = [
+			{id: 1, name:'Truong Quoc Bao', cId: 3},
+			{id: 2, name:'Nguyen Gia Huy', cId: 3},
+			{id: 3, name:'Tran Quoc Khanh', cId: 3},
+			{id: 4, name:'Nguyen Thi Ngoc Hue', cId: 2},
+			{id: 5, name:'Vo Ngoc Tan', cId: 2},
+			{id: 6, name:'Dang Ngoc Liem', cId: 1},
+			{id: 7, name:'Nguyen Quoc Trong', cId: 1},
 		];
-
+var classes =  [
+			{id: 1, class:'12A1'},
+			{id: 2, class:'12A2'},
+			{id: 3, class:'12A3'},
+		]
+//--Set view engine Pug
 app.set('view engine','pug');
 app.set('views','./views');
-
+//-Use body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.get('/', (req, res) => {
 	res.render('index', {
-		classes: '12A3',
+		classes: classes
 	});
 });
 
 app.get('/students', (req, res) => {
+
 	res.render('students/index', {
-		users: users,
+		students: students,
 	});
 });
 
 app.get('/students/Search', (req, res) => {
-	var matchedResult = users.filter((user) => {		
-		return user.name.toLowerCase().indexOf(req.query.fname.toLowerCase()) > -1;
+	var matchedResult = students.filter((student) => {		
+		return student.name.toLowerCase().indexOf(req.query.fname.toLowerCase()) > -1;
 	})
 	res.render('students/search', {
 		students: matchedResult
 	});
 });
+
+app.get('/students/create', (req, res) => {
+	res.render('students/create')
+})
+
+app.post('/students/create', (req, res) => {
+	students.push(req.body);
+	res.redirect('/students');
+})
 app.listen(port, () => {
 	console.log('Server listening on port ' + port);
 });
